fdac0f597d8df103ca78ba0f017d9fb0a5c67389
WonJeong <liclem@naver.com>
2025-03-19 15:18:02 +0900

    Add Input gameplay tags

---
+++ Source/CombatRPG/Private/CombatGameplayTags.cpp
@@ -0,0 +1,11 @@
+// liclem97 All Rights Reserved.
+
+
+#include "CombatGameplayTags.h"
+
+namespace CombatGameplayTags
+{
+	/** Input Tags **/
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look")
+}
---
+++ Source/CombatRPG/Public/CombatGameplayTags.h
@@ -0,0 +1,12 @@
+// liclem97 All Rights Reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace CombatGameplayTags
+{
+	/** Input Tags **/
+	COMBATRPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	COMBATRPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}

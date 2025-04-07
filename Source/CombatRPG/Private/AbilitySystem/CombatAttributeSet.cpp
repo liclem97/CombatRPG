90d3a448fb582b70a54abfe48874a05415e0b0f4
WonJeong <liclem@naver.com>
2025-04-07 19:24:43 +0900

    Init Attribute set

--- Source/CombatRPG/Private/AbilitySystem/CombatAttributeSet.cpp
+++ Source/CombatRPG/Private/AbilitySystem/CombatAttributeSet.cpp
@@ -3,3 +3,12 @@
 
 #include "AbilitySystem/CombatAttributeSet.h"
 
+UCombatAttributeSet::UCombatAttributeSet()
+{
+	InitCurrentHealth(1.f);
+	InitMaxtHealth(1.f);
+	InitCurrentRage(1.f);
+	InitMaxRage(1.f);
+	InitAttackPower(1.f);
+	InitDefensePower(1.f);
+}
--- Source/CombatRPG/Public/AbilitySystem/CombatAttributeSet.h
+++ Source/CombatRPG/Public/AbilitySystem/CombatAttributeSet.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystem/CombatAbilitySystemComponent.h"
 #include "CombatAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -14,4 +21,30 @@ class COMBATRPG_API UCombatAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UCombatAttributeSet();
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(UCombatAttributeSet, CurrentHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData MaxtHealth;
+	ATTRIBUTE_ACCESSORS(UCombatAttributeSet, MaxtHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData CurrentRage;
+	ATTRIBUTE_ACCESSORS(UCombatAttributeSet, CurrentRage)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData MaxRage;
+	ATTRIBUTE_ACCESSORS(UCombatAttributeSet, MaxRage)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UCombatAttributeSet, AttackPower)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(UCombatAttributeSet, DefensePower)
 };

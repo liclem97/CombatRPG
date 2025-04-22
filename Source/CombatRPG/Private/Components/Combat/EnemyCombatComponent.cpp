8b58d64db7b3b3fceb40b596ff1a230f4706eab6
WonJeong <liclem@naver.com>
2025-04-22 16:57:22 +0900

    Send notify melee hit

--- Source/CombatRPG/Private/Components/Combat/EnemyCombatComponent.cpp
+++ Source/CombatRPG/Private/Components/Combat/EnemyCombatComponent.cpp
@@ -3,12 +3,42 @@
 
 #include "Components/Combat/EnemyCombatComponent.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "CombatGameplayTags.h"
+
 #include "CombatDebugHelper.h"
 
 void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
-	if (HitActor)
+	if (OverlappedActors.Contains(HitActor)) return;
+
+	OverlappedActors.AddUnique(HitActor);
+
+	// TODO : block 체크 구현
+	bool bIsValidBlock = false;
+
+	const bool bIsPlayerBlocking = false;
+	const bool bIsMyAttackUnblockable = false;
+
+	if (bIsPlayerBlocking && !bIsMyAttackUnblockable)
 	{
-		Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" is hitting ") + HitActor->GetActorNameOrLabel());
+		// TODO : 블록이 올바른지 확인
+	}
+
+	FGameplayEventData EventData;
+	EventData.Instigator = GetOwningPawn();
+	EventData.Target = HitActor;
+
+	if (bIsValidBlock)
+	{
+		// TODO : 블록 성공
+	}
+	else
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			GetOwningPawn(),
+			CombatGameplayTags::Shared_Event_MeleeHit,
+			EventData
+		);
 	}
 }
